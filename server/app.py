356a64ae457890034ed839820c312a7f97fe7be7
Chris Lozzi <clozzi12@gmail.com>
2024-01-12 17:42:03 -0500

    all tests passing

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return f'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:str>')
+def print_string(str):
+    print(f'{str}')
+    return f'{str}'
+
+@app.route('/count/<int:num>')
+def count_int(num):
+    count = f''
+    for n in range(num):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def calculate(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return "Invalid operation"
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
